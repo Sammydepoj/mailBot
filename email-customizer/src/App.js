1a72f082ced934d95ced9b9a979df666d9aa552e
Sammydepoj <samueladepoju86@yahoo.com>
2022-11-24 21:53:06 +0100

    feat:react application skeleton created with react-router-dom installed

---
+++ email-customizer/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className='welcome'>
+          Welcome to <h1>mailBot</h1>
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
