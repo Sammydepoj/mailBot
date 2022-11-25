72b515a170a1e9d7f109ec5d99cf4c0aa125cbc5
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-11-25 17:08:30 +0100

    add data to ls

--- email-customizer/src/pages/ComposeMail/Compose.jsx
+++ email-customizer/src/pages/ComposeMail/Compose.jsx
@@ -1,30 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './Compose.module.css'
+import { useAppContext } from '../../Context/Context'
 
 const Compose = () => {
+    const { data, getLs, setLs, handleChange, handlegenerateVariant } = useAppContext()
+    useEffect(() => {
+        getLs();
+    }, [getLs])
+    
   return (
     <div className={styles.compose}>
         <div className={styles.mailBody}>
             <div className={styles.formInput}>
                 <p>From:</p>
-                <input type="text" placeholder='markessin@gmail.com'/>
+                <input type="text" placeholder='markessin@gmail.com' 
+                readOnly={true}
+                name="email"
+                defaultValue={data.email}/>
             </div>
 
             <div className={styles.hrLine}></div>
 
             <div className={styles.subjectInput}>
                 <p>Subject:</p>
-                <input type="text" placeholder='The real estate project'/>
+                <input type="text" placeholder='The real estate project'
+                    onChange={(e) => {
+                        handleChange(e);
+                        setLs();
+                    }}
+                    defaultValue={data.sub}
+                    name="sub"
+                />
             </div>
 
             <div className={styles.hrLine}></div>
 
             <div className={styles.textarea}>
-                <textarea placeholder='Your message' />
+                <textarea placeholder='Your message'
+                value={data.msg} name="msg" onChange={(e) => {
+                        handleChange(e);
+                        setLs();
+                    }} />
             </div>
 
             <div className={styles.btn}>
-                <button>Generate Email Variants</button>
+                <button
+                    onClick={handlegenerateVariant}
+                >Generate Email Variants</button>
             </div>
         </div>
     </div>
--- email-customizer/src/pages/Home/Home.jsx
+++ email-customizer/src/pages/Home/Home.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import { React } from 'react'
 import styles from './Home.module.css'
+import { useAppContext } from '../../Context/Context'
+
 
 const Home = () => {
+
+    const { error, data, handleNavigate, handleChange } = useAppContext()
+
     return (
         <div className={styles.container}>
             <div>
                 <h1>Add a Valid Email</h1>
 
                 <div className={styles.formContainer}>
-                    <input type="text" />
-                    <input type="submit" value='Continue' />
+                    <input type="text"
+                        name='email'
+                        value={data.email}
+                        onChange={handleChange}
+                    />
+                    <input type="submit" value='Continue'
+                        onClick={handleNavigate}
+                    />
                 </div>
+                {error ? <p>Please insert a valid mail</p> : ''}
             </div>
         </div>
     )
