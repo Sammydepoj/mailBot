ccc879a20586cfad0886b275267e9d84227d3b79
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-11-25 12:54:10 +0100

    feat: add styling and components

--- email-customizer/src/App.js
+++ email-customizer/src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 
 import Header from './components/header/Header';
 import Home from './pages/Home/Home';
+import Compose from './pages/ComposeMail/Compose';
+import Variant from './pages/Variant/Variant';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='compose' element={<Compose />} />
+        <Route path='/variant' element={<Variant />} />
       </Routes>
     </>
   );
